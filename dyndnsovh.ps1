cba5fb91ec4f2abca1a918b67ed1ae54ff3bf865
huleinte <huleinpylo@gmail.com>
2021-01-15 10:09:16 +0100

    Create dyndnsovh.ps1
    script to update

---
+++ dyndnsovh.ps1
@@ -0,0 +1,22 @@
+#RW5qb3kgdGhlIGNvZGU=
+function get-publicip
+{return (curl ifconfig.me/ip -UseBasicParsing).Content 
+}
+function get-Konibodns
+{
+    $OVHUSER = "konibo.info-cabdrpinto"
+    $OVHPASS = "Ko75016dL"
+     $password = ConvertTo-SecureString $OVHPASS -AsPlainText -Force;
+     return New-Object System.Management.Automation.PSCredential ($OVHUSER, $password) 
+}
+$cred = get-Konibodns
+# initialisation des variables
+
+$currentIP = get-publicip
+$OVHUSER = "USERNAME_Bob" # to change 
+$OVHPASS = "PASSWORD" #// to change
+$OVHDYNHOST = "URLexmaple.com" # TO Change
+
+$v=Invoke-WebRequest -uri "https://www.ovh.com/nic/update?myip=$($currentIP)`&hostname=$($OVHDYNHOST)`&system=dyndns" -Method GET -Credential $cred -UseBasicParsing
+Write-Output $v
+Read-Host -Prompt "Tada" 
